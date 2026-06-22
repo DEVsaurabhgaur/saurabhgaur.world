43857e3c52002a751494f4217663412bbdf2e83f
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-06-22 22:54:10 +0530

    perf: add dns-prefetch for Supabase CDN to reduce art image load latency

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+        {/* Preconnect to Supabase CDN for faster art image loads */}
+        <link rel="dns-prefetch" href="https://faqatqoixobqiwirjroc.supabase.co" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
